95e5070900c4d6c4b570772206b1798fb21c49f2
Dubey <anu.dubey@accenture.com>
2022-09-02 12:03:00 +0530

    trainig_session_Assignments

--- string_Assignment.php
+++ string_Assignment.php
@@ -1,8 +1,8 @@
 <?php
   $original_value = readline("Enter Original String :");
- $replace_value = readline("Enter String to replace :");
+  $replace_value = readline("Enter String to replace :");
   $start_position = readline("Enter Start Position : ");
   $end_Position = readline("Enter End Position : ");
-echo substr_replace($original_value,$replace_value,5,0).PHP_EOL;
+  echo substr_replace($original_value,$replace_value,$start_position,$end_Position).PHP_EOL;
 
 ?>
